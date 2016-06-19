81d7c82c40686006d59878a75d10797a1a2a5a94
niwoshijie <me591091298@126.com>
2016-06-19 19:56:44 +0800

    蚂蚁一小步之2016/6/19

--- app/src/main/java/mainpagers/mpackage/MPager.java
+++ app/src/main/java/mainpagers/mpackage/MPager.java
@@ -2,6 +2,7 @@ package mainpagers.mpackage;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import org.xutils.view.annotation.ViewInject;
@@ -22,17 +23,24 @@ public class MPager extends BasePager {
     @ViewInject(R.id.mao_recycle)
     private RecyclerView twoWayView;
 
-
     @Override
     public View initView() {
-        View view =View.inflate(context, R.layout.m_layout,null);
+        View view = View.inflate(context, R.layout.m_layout,null);
         x.view().inject(this,view);
         return view;
     }
 
+    private String[]  resourse = new String[]{"美女1","美女2","美女3"};
+    private int[]  resourseImage = new int[]{R.mipmap.h1,R.mipmap.h2,R.mipmap.h3};
 
     @Override
     public void initData() {
+        twoWayView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        MaoAdapter maoAdapter = new MaoAdapter(resourse,resourseImage);
+        twoWayView.setAdapter(maoAdapter);
+        //设置item之间的间隔
+        SpacesItemDecoration decoration=new SpacesItemDecoration(16);
+        twoWayView.addItemDecoration(decoration);
 
     }
 
--- app/src/main/java/mainpagers/mpackage/MaoAdapter.java
+++ app/src/main/java/mainpagers/mpackage/MaoAdapter.java
@@ -1,15 +1,13 @@
 package mainpagers.mpackage;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
-
-import java.util.List;
 
 import liushaobo.mad.R;
 
@@ -18,44 +16,47 @@ import liushaobo.mad.R;
  */
 public class MaoAdapter extends RecyclerView.Adapter<MaoAdapter.MaoRecycleHolder>{
 
-    private List<String> dataString;//文本信息
-    private int[] images;
-    private Context context;
-    public void setResourse(List<String> textString,int[] image,Context context){
-        this.dataString = dataString;
-        this.images = image;
-        this.context = context;
+    private String[]  resourseText;
+    private int[]  resourseImage;
+
+    public MaoAdapter(String[] resourseText, int[] resourseImage) {
+        this.resourseText = resourseText;
+        this.resourseImage = resourseImage;
     }
 
     @Override
     public MaoRecycleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = View.inflate(context, R.layout.mao_pager_item,null);
+        View view = View.inflate(parent.getContext(), R.layout.mao_pager_item,null);
         return new MaoRecycleHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MaoRecycleHolder holder, int position) {
-
-      }
+        holder.setData(position);
+    }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return resourseText.length;
     }
 
     public class MaoRecycleHolder extends RecyclerView.ViewHolder{
 
 
-        @ViewInject(R.id.mao_pager_text)
+        @ViewInject(R.id.mao_pager_item_tv)
         private TextView mao_pager_text;
 
+        @ViewInject(R.id.mao_pager_item_iamge)
+        private ImageView mao_pager_item_iamge;
 
         public MaoRecycleHolder(View itemView) {
             super(itemView);
             x.view().inject(this,itemView);
         }
 
-        public void setData(){
+        public void setData(int itemPosition){
+            mao_pager_text.setText(resourseText[itemPosition]);
+            mao_pager_item_iamge.setImageResource(resourseImage[itemPosition]);
 
         }
 
